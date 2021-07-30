9505c78cddedd3617c8a948eeaa447a1e05e4c9c
Brama Udi <bramaudi@users.noreply.github.com>
2021-07-30 07:44:43 +0700

    🐛 fix: FAB inherit props

--- src/components/FloatActionButton.jsx
+++ src/components/FloatActionButton.jsx
@@ -1,8 +1,8 @@
 import iconPlus from '../assets/icons/plus.svg'
 
-const FloatActionButton = () => {
+const FloatActionButton = (props) => {
 	return (
-		<div className="fixed right-7 bottom-7 opacity-80 cursor-pointer">
+		<div {...props} className="fixed right-7 bottom-7 opacity-70 cursor-pointer">
 			<div className="p-2 rounded-full bg-blue-500 hover:bg-blue-700 text-gray-300">
 				<img className="w-10 h-10 invert" src={iconPlus} alt="+" />
 			</div>
