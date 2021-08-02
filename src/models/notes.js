3ab3aa745105c56d52e9a3fd38532f82f7c8a32f
Brama Udi <bramaudi@users.noreply.github.com>
2021-08-02 18:32:13 +0700

    💡 add: jsdoc types

--- src/models/notes.js
+++ src/models/notes.js
@@ -13,17 +13,31 @@ const structure = {
 	user_id: auth?.id || ''
 }
 
+/**
+ * Order notes[] by updated_at, created_at descending
+ */
 const order = (a, b) => {
 	return new Date(b.updated_at) - new Date(a.updated_at)
 			|| new Date(b.created_at) - new Date(a.created_at)
 }
 
+/**
+ * Decrypt title / body of note
+ * @param {structure} note Encrypted note
+ * @returns {structure} Decrypted note
+ */
 const decryptNote = (note) => {
-	const title = decrypt(note.title, auth.id)
-	const body = decrypt(note.body, auth.id)
+	const title = note.title ? decrypt(note.title, auth.id) : ''
+	const body = note.body ? decrypt(note.body, auth.id) : ''
 	return {...note, title, body}
 }
 
+/**
+ * List of notes
+ * @param {Number} lastId Last notes id
+ * @param {Number} limit Number of notes
+ * @returns {Promise}
+ */
 const index = async (lastId = 0, limit = 10) => {
 	return await supabase
 		.from('notes')
@@ -34,6 +48,11 @@ const index = async (lastId = 0, limit = 10) => {
 		.limit(limit)
 }
 
+/**
+ * Read single note
+ * @param {Number} id Note's id
+ * @returns {Promise}
+ */
 const read = async (id) => {
 	return await supabase
 		.from('notes')
@@ -42,28 +61,43 @@ const read = async (id) => {
 		.single()
 }
 
+/**
+ * Store new note
+ * @param {structure} data Note
+ * @returns {Promise}
+ */
 const store = async (data) => {
-	data.body = encrypt(data.body, auth.id)
-	data.title = encrypt(data.title, auth.id)
+	if (data.body !== '') data.body = encrypt(data.body, auth.id)
+	if (data.title !== '') data.title = encrypt(data.title, auth.id)
 	return await supabase
 		.from('notes')
 		.insert(data)
 }
 
+/**
+ * Update note
+ * @param {structure} data Note
+ * @returns {Promise}
+ */
 const update = async (data) => {
-	data.body = encrypt(data.body, auth.id)
-	data.title = encrypt(data.title, auth.id)
+	if (data.body !== '') data.body = decrypt(data.body, auth.id)
+	if (data.title !== '') data.title = decrypt(data.title, auth.id)
 	return await supabase
 		.from('notes')
 		.update(data)
 		.match({ id: data.id })
 }
 
-const remove = async (noteId) => {
+/**
+ * Remove note
+ * @param {Number} id Note's id
+ * @returns 
+ */
+const remove = async (id) => {
 	return await supabase
 		.from('notes')
 		.delete()
-		.eq('id', noteId)
+		.eq('id', id)
 }
 
 export default {
