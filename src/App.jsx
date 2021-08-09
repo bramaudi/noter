a354beee2cb0f2b2c4d7a67da5f753a7af4611f1
Brama Udi <bramaudi@users.noreply.github.com>
2021-08-10 05:08:34 +0700

    ✨ feat(pwa): add offline capability

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import { NoteProvider } from './store/NoteContext'
+import { registerSW } from 'virtual:pwa-register'
+import { onMount } from 'solid-js';
 
 function App({ Routes }) {
+  onMount(() => {
+    if (typeof window !== 'undefined') {
+      registerSW({
+        onOfflineReady() {
+          console.log('App ready to use offline');
+        }
+      })
+    }
+  })
   return (
     <NoteProvider>
       <Routes />
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import Pages from "vite-plugin-pages";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,7 @@ export default defineConfig({
       importMode: 'async'
     }),
     solidPlugin(),
+    VitePWA()
   ],
   build: {
     target: "esnext",
