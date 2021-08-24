408bc25b9aab0d3e2674ae200362d2ac05b2afb8
Brama Udi <bramaudi@users.noreply.github.com>
2021-08-24 11:18:51 +0700

    🚑 fix(pwa): use inject manifest from vite plugin

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,40 @@ export default defineConfig({
       importMode: 'async'
     }),
     solidPlugin(),
-    VitePWA()
+    VitePWA({
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,svg,png,jpg}',
+        ]
+      },
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Noter',
+        short_name: 'Noter',
+        description: 'PWA Notes',
+        icons: [
+          {
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      }
+    }),
   ],
   server: {
     host: '0.0.0.0'
