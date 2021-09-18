09c9541f3051b056bfe9af4007199ef938b1045a
Brama Udi <bramaudi.al@gmail.com>
2021-09-18 20:52:43 +0700

    style(landing-page): add wavy divider

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import { createEffect } from 'solid-js'
 import supabase, { auth } from '../services/supabase'
 import { notesFetchAll } from '../models/notes-dummy'
 // Components
+import divider from '../assets/divider.svg'
 import iconGithub from '../assets/icons/github.svg'
 import iconCheck from '../assets/icons/check.svg'
 import { Link } from 'solid-app-router'
@@ -57,9 +58,47 @@ const LandingPage = () => {
 	})
 
 	return (
-		<>
+		<div className="w-full bg-white">
+			{/* Hero */}
+			<div className="max-w-3xl mx-auto pb-10 sm:pb-0 sm:flex sm:items-center">
+
+				<div className="flex-1 p-10 py-20">
+					<div className="text-5xl font-semibold">Noter</div>
+					<div className="mt-2">Free open source simple taking note's app with cloud backup & sync, work offline and PWA installable.</div>
+					<div className="mt-5 flex items-center">
+						<Link
+							href="/notes"
+							className="p-2 px-3 rounded bg-green-700 hover:bg-green-900 text-gray-100"
+							>
+							Getting started
+						</Link>
+
+						<a
+							href="//github.com/bramaudi/noter"
+							target="_blank"
+							className="p-2 px-3 ml-3 flex items-center rounded bg-gray-300 hover:bg-gray-400 text-black"
+						>
+							<img className="w-4 mr-2" src={iconGithub} alt="github" />
+							<div>Give a star</div>
+						</a>
+					</div>
+				</div>
+
+				<div className="flex-1 px-10 pt-15">
+					<FeatureCheck>Free & open-source</FeatureCheck>
+					<FeatureCheck tips="Install like native app & work offline">Progressive Web Apps-ready</FeatureCheck>
+					<FeatureCheck>Backup or Sync notes</FeatureCheck>
+					<FeatureCheck tips="Use it without login">Anonymous user</FeatureCheck>
+					<FeatureCheck>Simple & Responsive UI</FeatureCheck>
+				</div>
+
+			</div>
+			{/* Divider */}
+			<div className="-my-1 w-full">
+				<img src={divider} alt="Divider" className="w-full" />
+			</div>
 			{/* Ranomize */}
-			<div className="p-5 sm:p-10 h-60 overflow-hidden bg-gradient-to-t from-white to-green-700">
+			<div className="p-5 pt-14 h-96 sm:h-72 overflow-hidden bg-green-700">
 				<div className="max-w-3xl mx-auto">
 					<Masonry
 						breakpointCols={breakpointColumnsObj}
@@ -97,41 +136,7 @@ const LandingPage = () => {
 					</Masonry>
 				</div>
 			</div>
-			{/* Hero */}
-			<div className="max-w-3xl mx-auto pb-10 sm:pb-0 sm:flex sm:items-center">
-
-				<div className="flex-1 p-10 py-20">
-					<div className="text-5xl font-semibold">Noter</div>
-					<div className="mt-2">Free open source simple taking note's app with cloud backup & sync, work offline and PWA installable.</div>
-					<div className="mt-5 flex items-center">
-						<Link
-							href="/notes"
-							className="p-2 px-3 rounded bg-green-700 hover:bg-green-900 text-gray-100"
-							>
-							Getting started
-						</Link>
-
-						<a
-							href="//github.com/bramaudi/noter"
-							target="_blank"
-							className="p-2 px-3 ml-3 flex items-center rounded bg-gray-300 hover:bg-gray-400 text-black"
-						>
-							<img className="w-4 mr-2" src={iconGithub} alt="github" />
-							<div>Give a star</div>
-						</a>
-					</div>
-				</div>
-
-				<div className="flex-1 px-10 pt-15">
-					<FeatureCheck>Free & open-source</FeatureCheck>
-					<FeatureCheck tips="Install like native app & work offline">Progressive Web Apps-ready</FeatureCheck>
-					<FeatureCheck>Backup or Sync notes</FeatureCheck>
-					<FeatureCheck tips="Use it without login">Anonymous user</FeatureCheck>
-					<FeatureCheck>Simple & Responsive UI</FeatureCheck>
-				</div>
-
-			</div>
-		</>
+		</div>
 	)
 }
 
